b3222e4893b96922c89effac8f48c7e20dcea382
Tim Gross <tim@0x74696d.com>
2015-11-30 10:05:54 -0500

    Add liveness polling to example/start.sh

--- example/start.sh
+++ example/start.sh
@@ -9,14 +9,25 @@ export NGINX_CONF="$(cat ./nginx.ctmpl)"
 
 docker-compose up -d
 
-consoles() {
-    local CONSUL=$(getIpPort consul 8500)
-    local NGINX=$(getIpPort nginx 80)
-
-    echo 'Opening Consul console...'
-    open http://${CONSUL}/ui/
-    echo 'Opening web page... the page will reload every 5 seconds with any updates.'
-    open http://${NGINX}/app/
+poll-for-page() {
+    echo "$2"
+    while :
+    do
+        curl --fail -s -o /dev/null "$1" && break
+        sleep 1
+        echo -ne .
+    done
+    echo
+    echo "$3"
+    open "$1"
 }
 
-consoles
+# poll Consul for liveness and then open the console
+poll-for-page "http://$(getIpPort consul 8500)/ui/" \
+              'Waiting for Consule...' \
+              'Opening Consul console... Refresh the page to watch services register.'
+
+# poll Nginx for liveness and then open the page
+poll-for-page "http://$(getIpPort nginx 80)/app/" \
+              'Waiting for application to register as healthy...' \
+              'Opening web page... The page will reload every 5 seconds with any updates.'
