b65eafca9e847cc4e5929a86a7289ac18bae9c33
Jason Pincin <jason@pincin.com>
2016-10-03 22:10:05 -0400

    Verify challenge token is written to all nginx containers before accepting ACME challenge

--- etc/acme/dehydrated/hook.sh
+++ etc/acme/dehydrated/hook.sh
@@ -4,23 +4,36 @@ if [ "${CONSUL_AGENT}" = "" -a "${CONSUL}" != "" ]; then
     CONSUL_HOST_DEFAULT=${CONSUL}
 fi
 CONSUL_HOST=${CONSUL_HOST:-$CONSUL_HOST_DEFAULT}
-CONSUL_ROOT="http://${CONSUL_HOST}:8500/v1/kv/nginx"
+CONSUL_ROOT="http://${CONSUL_HOST}:8500/v1"
+CONSUL_KEY_ROOT="${CONSUL_ROOT}/kv/nginx"
 CHALLENGE_PATH="/.well-known/acme-challenge"
 
 function deploy_challenge {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+    local TOKEN_DEPLOY_RETRY_LIMIT=6
 
-    curl -sX PUT -d "${TOKEN_FILENAME}" ${CONSUL_ROOT}/acme/challenge/token-filename | log
+    curl -sX PUT -d "${TOKEN_FILENAME}" ${CONSUL_KEY_ROOT}/acme/challenge/token-filename | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX PUT -d "${TOKEN_VALUE}" ${CONSUL_ROOT}/acme/challenge/token-value | log
+    curl -sX PUT -d "${TOKEN_VALUE}" ${CONSUL_KEY_ROOT}/acme/challenge/token-value | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
 
     # verify all nginx containers are responding with challenge before continuing
-    # TODO: this is currently only looking at localhost
-    RETRIES=0
+    local NGINX_INSTANCES=$(curl -s "${CONSUL_ROOT}/catalog/service/nginx" | jq -Mr '.[].ServiceAddress')
+    local NGINX_INSTANCE_COUNT=$(echo "${NGINX_INSTANCES}" | wc -l)
+    local RETRIES=0
+    local MATCHING=0
     printf " + Waiting for challenge to be deployed..."
-    while [ "$(curl -s --header \"HOST: ${DOMAIN}\" http://localhost${CHALLENGE_PATH}/${TOKEN_FILENAME})" != "${TOKEN_VALUE}" -a $RETRIES -lt 6 ]; do
+    while [ $RETRIES -lt $TOKEN_DEPLOY_RETRY_LIMIT ]; do
+        for NGINX_INSTANCE_HOST in $NGINX_INSTANCES; do
+            if [ "$(curl -s --header \"HOST: ${DOMAIN}\" http://${NGINX_INSTANCE_HOST}${CHALLENGE_PATH}/${TOKEN_FILENAME})" != "${TOKEN_VALUE}" ]; then
+                MATCHING=$((MATCHING+1))
+            fi
+        done
+        if [ $MATCHING -eq $NGINX_INSTANCE_COUNT ]; then
+            break
+        fi
         RETRIES=$((RETRIES+1))
+        MATCHING=0
         printf "."
         sleep 2
     done
@@ -30,28 +43,28 @@ function deploy_challenge {
 function clean_challenge {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 
-    curl -sX DELETE ${CONSUL_ROOT}/acme/challenge/token-filename | log
+    curl -sX DELETE ${CONSUL_KEY_ROOT}/acme/challenge/token-filename | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX DELETE ${CONSUL_ROOT}/acme/challenge/token-value | log
+    curl -sX DELETE ${CONSUL_KEY_ROOT}/acme/challenge/token-value | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX PUT -d "${TOKEN_FILENAME}" ${CONSUL_ROOT}/acme/challenge/last-token-filename | log
+    curl -sX PUT -d "${TOKEN_FILENAME}" ${CONSUL_KEY_ROOT}/acme/challenge/last-token-filename | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
 }
 
 function deploy_cert {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
 
-    curl -sX PUT -d "$(cat ${KEYFILE})" ${CONSUL_ROOT}/acme/key | log
+    curl -sX PUT -d "$(cat ${KEYFILE})" ${CONSUL_KEY_ROOT}/acme/key | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX PUT -d "$(cat ${CERTFILE})" ${CONSUL_ROOT}/acme/cert | log
+    curl -sX PUT -d "$(cat ${CERTFILE})" ${CONSUL_KEY_ROOT}/acme/cert | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX PUT -d "$(cat ${CHAINFILE})" ${CONSUL_ROOT}/acme/chain | log
+    curl -sX PUT -d "$(cat ${CHAINFILE})" ${CONSUL_KEY_ROOT}/acme/chain | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX PUT -d "$(cat ${FULLCHAINFILE})" ${CONSUL_ROOT}/acme/fullchain | log
+    curl -sX PUT -d "$(cat ${FULLCHAINFILE})" ${CONSUL_KEY_ROOT}/acme/fullchain | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX PUT -d "${TIMESTAMP}" ${CONSUL_ROOT}/acme/timestamp | log
+    curl -sX PUT -d "${TIMESTAMP}" ${CONSUL_KEY_ROOT}/acme/timestamp | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
-    curl -sX PUT -d "$(date +%s)" ${CONSUL_ROOT}/acme/touched | log
+    curl -sX PUT -d "$(date +%s)" ${CONSUL_KEY_ROOT}/acme/touched | log
     test ${PIPESTATUS[0]} -eq 0 || (log "${FUNCNAME} failed" && return)
 }
 
