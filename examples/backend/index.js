a41c023f093621ad6a97c899060b066451ecd766
Tim Gross <tim@0x74696d.com>
2017-02-06 16:42:45 -0500

    Fix bugs in example CNS naming and Consul setup

--- examples/backend/index.js
+++ examples/backend/index.js
@@ -3,11 +3,12 @@
 // Load modules
 
 const Http = require('http');
+const Os = require('os');
 
 
 const server = module.exports = Http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello World\n');
+  res.end('Hello World\n' + Os.hostname() + '\n');
 });
 
 server.listen(3001, () => {
